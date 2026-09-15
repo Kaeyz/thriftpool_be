d9169e3bda7de59caf087a710ca0d8ca313da8b6
Agbele Kolade <kolagbele@gmail.com>
2026-09-15 03:54:50 +0200

    updated cors config

--- apps/community/src/runtime/http/config/cors-config.ts
+++ apps/community/src/runtime/http/config/cors-config.ts
@@ -1,12 +1,12 @@
 import { getKeys } from "@/config/keys";
 
-const prodOrigins = ["https://account.thriftpool.com"];
+const prodOrigins = ["https://thriftpool.com"];
 
 const testOrigins = [
   "http://localhost:3000",
   "http://localhost:4000",
-  "https://dev-account-thriftpool.up.railway.app",
-  "https://dev-account.thriftpool.com",
+  "https://staging-cp-api.up.railway.app",
+  "https://staging-cp-fe.up.railway.app",
 ];
 
 export const allowedOrigins = getKeys().appEnv === "prod" ? prodOrigins : testOrigins;
