4a27171b2d19553d0384d7cf9cced02465c64462
Agbele Kolade <kolagbele@gmail.com>
2026-09-25 10:43:59 +0100

    added user module index

--- apps/cp-server/src/modules/users/index.ts
+++ apps/cp-server/src/modules/users/index.ts
@@ -1,4 +1,5 @@
 export * from "./services/user.service";
 export * from "./services/user-account.service";
+export * from "./services/user.upload-config";
 export * from "./api/user.schema";
 export * from "./api/user.routes";
